dacef21521890e963f5525a4eeecbf2e8479fa87
wanghao <admin@wang-hao.com>
2021-11-05 21:55:51 +0800

    11.5

--- src/main/java/gomoku/Controller/LoginController.java
+++ src/main/java/gomoku/Controller/LoginController.java
@@ -6,11 +6,17 @@ import gomoku.Main;
 import gomoku.SaveLoad.Load;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class LoginController {
 
@@ -22,13 +28,13 @@ public class LoginController {
     private TextField nameT;
 
     @FXML
-    private TextField passnumT;
+    private PasswordField passT;
 
 
 
     Load load=new Load();
     public void login(ActionEvent e) throws IOException {
-        Player player=load.loadPlayer(nameT.getText(),passnumT.getText());
+        Player player=load.loadPlayer(nameT.getText(),passT.getText());
         if (player!=null) {
            /* Main.anchorPane = new AnchorPane();
             Main.anchorPane.setStyle("-fx-background-color:#E5C992;");
@@ -41,10 +47,24 @@ public class LoginController {
             Main.primaryStage.setHeight(850);
             Main.primaryStage.setResizable(false);
             Main.primaryStage.show();*/
-            Main.player=player;
-            PlayerController playerController=new PlayerController();
+            if("123".equals(player.getPassnum())){
+                Parent parent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/View/Resetpassword.fxml")));
+                Scene scene = new Scene(parent);
+                Main.primaryStage.setScene(scene);
+
+
+
+            }else{Main.player=player;
+                PlayerController playerController=new PlayerController();}
+
+
+
         }else if(player==null){
-            System.out.println("sl");
+//            System.out.println("sl");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.titleProperty().set("提示");
+            alert.headerTextProperty().set("输入的账号或密码有误请重新输入");
+            alert.showAndWait();
         }
     }
 }
